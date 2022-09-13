8c71208c80c27dc46dd280ba5d86826de520b6c4
Ujjwal Kumar <ujjwalkumar@icloud.com>
2022-09-12 22:43:00 -0700

    Update mmm_code.py

--- mmm_code.py
+++ mmm_code.py
@@ -6,6 +6,14 @@ numbmean = numbsum / numblen
 print("The mean of all the listed numbers (", numblist, ") is", str(numbmean))
 
 # Median
+numblist = [2, 4, 5, 8, 9, 10]
+numblen = len(numblist)
+numblist.sort()
+if numblen % 2 == 0:
+  median1 = numblist[numblen // 2]
+  median2 = numblist[numblen // 2-1]
+  numbmedian = (median1+median2)/2
+print("The median of all the listed numbers (", numblist, ") is", str(numbmedian))
 
 # Mode
 
