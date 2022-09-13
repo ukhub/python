c3f8e1165ab83179edb4646b35c20d07d11e2a03
Ujjwal Kumar <ujjwalkumar@icloud.com>
2022-09-12 22:35:55 -0700

    Create mmm_code.py

---
+++ mmm_code.py
@@ -0,0 +1,11 @@
+# Mean = Average value of all numbers
+numblist = [2, 4, 5, 8, 9, 10]
+numblen = len(numblist)
+numbsum = sum(numblist)
+numbmean = numbsum / numblen
+print("The mean of all the listed numbers (", numblist, ") is", str(numbmean))
+
+# Median
+
+# Mode
+
