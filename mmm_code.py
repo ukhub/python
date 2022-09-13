b2d0a02856688fbf537443cd8734376b78f44816
Ujjwal Kumar <ujjwalkumar@icloud.com>
2022-09-12 22:49:11 -0700

    Update mmm_code.py

--- mmm_code.py
+++ mmm_code.py
@@ -16,4 +16,15 @@ if numblen % 2 == 0:
 print("The median of all the listed numbers (", numblist, ") is", str(numbmedian))
 
 # Mode
+from collections import Counter
+numblist = [2, 4, 4, 5, 8, 9, 10]
+numblen = len(numblist)
+val = Counter(numblist)
+findMode = dict(val)
+mode = [i for i, v in findMode.items() if v == max(list(val.values()))]  
+if len(mode) == numblen:
+    findMode = "The group of number do not have any mode"
+else:
+    findMode = "The mode of a number is / are: " + ', '.join(map(str, mode))
+print(findMode)
 
